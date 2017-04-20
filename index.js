5a39d7f1f139777a8e596144614440d96e3a94fe
YeoJiKim <850457666@qq.com>
2017-04-20 22:28:01 +0800

    change layout

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ var points = [];
 var metricPerPixel;
 var refLen;
 var lineLen;
-var img = new Image();
+var img;
 $(document).ready(function (){
-	var canvas = $("#picCanvas");
-	var cxt = canvas.get()[0].getContext('2d');
+	var canvas = $("#picCanvas")[0];
+	var cxt = canvas.getContext('2d');
+	//var canvas = document.getElementById("picCanvas");
+	//var cxt = canvas.getContext("2d");
+	
 	cxt.strokeStyle = "#f00";
 	cxt.lineWidth = 3;
+	cxt.font="bold 15px Arial";
+	cxt.fillStyle="#058";
+	img = new Image();
 	
-	Init();
+	Init(canvas);
 	drawLines();
 	$("#iptfileupload").change(function (){
-		points = [];
-        cxt.clearRect(0, 0, 400, 400);
+		staInit();
+        cxt.clearRect(0, 0, canvas.width, canvas.height);
         if (typeof FileReader === 'undefined') {
             alert('Your browser does not support FileReader...');
             return;
@@ -22,11 +28,11 @@ $(document).ready(function (){
 
         reader.onload = function (e) {    
 			
-            var cxt = document.getElementById("picCanvas").getContext("2d")
-			
             img.src = this.result;
             img.onload = function () {
-            cxt.drawImage(img, 0, 0);
+            cxt.drawImage(img, 0, 0,canvas.width,canvas.height);
+			console.log(img.width,img.height);
+			console.log(canvas.width,canvas.height);
             }
         }
         reader.readAsDataURL(document.getElementById(this.id).files[0]);
@@ -52,24 +58,52 @@ $(document).ready(function (){
 		if(points.length<2){
 			alert("Please choose reference in picture");
 		}
-		refLen = $("#reflen").val();
-		lineLen = calReferLine(x1,y1,x2,y2);
-		metricPerPixel = refLen/lineLen;
+		else{
+			lineLen = calReferLine(canvasToPicX(points[0].x),canvasToPicY(points[0].y),
+									canvasToPicX(points[1].x),canvasToPicY(points[1].y));
+			
+			refLen = $("#reflen").val();
+			if(refLen == 0 ||refLen == undefined || isNaN(refLen) ){
+				alert("Please enter reference length");
+			}
+			else{
+				cxt.fillText(refLen,(points[0].x + points[1].x) / 2 ,(points[0].y + points[1].y) / 2 );
+			}
+			  
+			metricPerPixel = lineLen / refLen;  
+		}
 	});
+	$("#res").click(function (){
+		staInit();
+		cxt.clearRect(0, 0, canvas.width, canvas.height);
+		
+		cxt.drawImage(img, 0, 0,canvas.width,canvas.height);
+   
+		
+	});
+	$("#sav").click(function (){
+		
+		Canvas2Image.saveAsJPEG(canvas, canvas.width, canvas.height);
+	});
+		
+	
 });
-function Init(){
-	picInit();
+function Init(c){
+	picInit(c);
 	staInit();
 }
-function picInit(){
-    var cxt = document.getElementById("picCanvas").getContext("2d")
+function picInit(c){
+    var cxt = c.getContext("2d");
     img.src = "1.jpg";
     img.onload = function () {
-		cxt.drawImage(img, 0, 0);
+		cxt.drawImage(img, 0, 0,c.width,c.height);
+		console.log(c.width,c.height);
     }
 }
 function staInit(){
 	points = [];
+	metricPerPixel = 0;
+	
 	var refInit =$(".form-control").val(0);
 	var refObject=$(".form-control").find("option:selected").val();
 		
@@ -100,7 +134,7 @@ function drawLines(){
     toY,
 	endX,
 	endY;
-
+	
 	function getCanvasPosX(canvas,e){	
 		var rect = canvas.getBoundingClientRect(); 
 		return e.clientX - rect.left * (canvas.width / rect.width);
@@ -115,9 +149,8 @@ function drawLines(){
 	canvas.addEventListener('mousedown', function (e) {
 		moveX = getCanvasPosX(canvas,e);
 		moveY = getCanvasPosY(canvas,e);
-		//img.onload = function () {
-		//	cxt.drawImage(img, 0, 0);
-		//}
+		
+		
 		canvas.addEventListener('mousemove', drawLine);
 		points.push({
 			x: moveX,
@@ -128,13 +161,21 @@ function drawLines(){
 	canvas.addEventListener('mouseup', function (e) {
 		endX = getCanvasPosX(canvas,e);
 		endY = getCanvasPosY(canvas,e);
-		//img.onload = function () {
-		//	cxt.drawImage(img, 0, 0);
-		//}
+		
 		points.push({
 			x: endX,
 			y: endY
 		});
+		
+		for(var i=3;i<points.length;i+=2){	
+				cxt.fillText((calReferLine(canvasToPicX(points[i-1].x),canvasToPicY(points[i-1].y),
+											canvasToPicX(points[i].x),canvasToPicY(points[i].y)) / metricPerPixel).toFixed(2) , 
+											(points[i-1].x + points[i].x) / 2 ,(points[i-1].y + points[i].y) / 2 );
+			}
+			
+	
+		
+		
 		canvas.removeEventListener('mousemove', drawLine);
 	
 	})
@@ -143,16 +184,45 @@ function drawLines(){
 		toX = getCanvasPosX(canvas,e);
 		toY = getCanvasPosY(canvas,e);
 	
-		cxt.clearRect(0, 0, 400, 400);		
-		cxt.drawImage(img, 0, 0);		
+		cxt.clearRect(0, 0, canvas.width, canvas.height);		
+		cxt.drawImage(img, 0, 0,canvas.width,canvas.height);		
 		cxt.beginPath();
 		cxt.moveTo(moveX, moveY);
 		cxt.lineTo(toX, toY);
+		
 		for(var i=1;i<points.length;i+=2){
 			cxt.moveTo(points[i-1].x,points[i-1].y);
 			cxt.lineTo(points[i].x,points[i].y);
+			
+			if(refLen == 0 ||refLen == undefined || isNaN(refLen) ){
+				alert("Please enter reference length");
+			}
+			else{
+				cxt.fillText(refLen,(points[0].x + points[1].x) / 2 ,(points[0].y + points[1].y) / 2 );
+				
+				if(i>=3){
+				cxt.fillText((calReferLine(canvasToPicX(points[i-1].x),canvasToPicY(points[i-1].y),
+											canvasToPicX(points[i].x),canvasToPicY(points[i].y)) / metricPerPixel).toFixed(2) , 
+										(points[i-1].x + points[i].x) / 2 ,(points[i-1].y + points[i].y) / 2 );
+			}
+			}		
 	
 		}	
 		cxt.closePath();
 		cxt.stroke();	
 	}}
+
+function canvasToPicX(m){
+	var wPicToCan = 300 / img.width;
+		
+	var mCanToPic = m / wPicToCan;
+		
+	return mCanToPic;
+} 
+function canvasToPicY(n){		
+	var hPicToCan = 400 / img.height;
+	
+	var nCanToPic = n / hPicToCan;
+		
+	return nCanToPic;
+} 
